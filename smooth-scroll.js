2e105b0c5765032aca68d11368b156d1c512b3c6
AL <powerbuoy@gmail.com>
2017-10-19 11:51:39 +0200

    Initial

---
+++ smooth-scroll.js
@@ -0,0 +1,50 @@
+(function (factory) {
+	'use strict';
+
+	if (typeof module === 'object' && typeof module.exports === 'object') {
+		module.exports = factory(require('jquery'));
+	}
+	else {
+		factory(jQuery);
+	}
+})(function ($) {
+	'use strict';
+
+	$.fn.smoothScroll = function (conf) {
+		var config = $.extend({
+			duration: 600,
+			offset: 0
+		}, conf);
+
+		return this.each(function () {
+			var trigger = $(this);
+			var href = trigger.attr('href');
+			var targetId = href.substr(href.indexOf('#')).substr(1);
+			var target = document.getElementById(targetId);
+
+			if (target) {
+				trigger.click(function (e) {
+					e.stopPropagation();
+					e.preventDefault();
+
+					var targetBCT = target[0].getBoundingClientRect();
+					var st = $(document).scrollTop();
+
+					window.location.hash = '#' + targetId; // Bump :target
+
+					$(document).scrollTop(st);
+
+					$('html, body').animate({scrollTop: targetBCT.top + config.offset}, config.duration);
+				});
+			}
+			else if (targetId == 'top') {
+				trigger.click(function (e) {
+					e.stopPropagation();
+					e.preventDefault();
+
+					$('html, body').animate({scrollTop: 0}, config.duration);
+				});
+			}
+		});
+	};
+});
